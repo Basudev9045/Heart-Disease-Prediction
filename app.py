c246fd9e9b1d5b4b8198242416bee1add6f04951
Basudev <pinkuyadav7295@gmail.com>
2026-06-18 01:54:29 +0530

    Initial heart disease prediction

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st 
+import pandas as pd
+import joblib
+
+model = joblib.load("logisticRegression_heart.pkl")
+scaler = joblib.load("scaler.pkl")
+excepted_columns = joblib.load("columns.pkl")
+
+st.title("Heart stroke prediction by Shubham singh")
+st.markdown("Provide the following details")
+
+age = st.slider("Age",18,100,40)
+sex = st.selectbox("SEX",['M','F'])
+chest_pain = st.selectbox("Chest Pain Type",["ATA","NAP","TA","ASY"])
+resting_bp = st.number_input("Resting Blood Pressure(mm Hg)",80,200,120)
+cholesterol = st.number_input("Cholesterol(mg/dL)",100,600,200)
+fasting_bs = st.selectbox("Fasting Blood Sugar>120 mg/dl",[0,1])
+resting_ecg = st.selectbox("Resting EGC", ["Normal","ST","LVH"])
+max_hr = st.slider("Max Heart Rate,",60,220,150)
+exeecise_angina = st.selectbox("Exercise-Induced Angina",["Y","N"])
+oldpeak = st.slider("Oldpeak (ST Depression)",0.0,6.0,1.0)
+st_slope = st.selectbox("ST Slope",["Up","Flat","Down"])
+
+if st.button("Predict"):
+    raw_input = {
+        'Age':age,
+        'RestingBP':resting_bp,
+        'Cholesterol':cholesterol,
+        'FastingBS':fasting_bs,
+        'Max_hr':max_hr,
+        'Oldpeak':oldpeak,
+        'SEX' + sex:1,
+        'ChestPainType' + chest_pain:1,
+        'Resting_ecg' + resting_ecg:1,
+        'Exeecise_angina' + exeecise_angina:1,
+        'St_slope' + st_slope:1
+
+    }
+    input_df =  pd.DataFrame([raw_input])
+
+    for col in excepted_columns:
+        if col not in input_df.columns:
+            input_df[col]=0
+    
+    input_df = input_df[excepted_columns]
+
+    scaled_input = scaler.transform(input_df)
+    prediction = model.predict(scaled_input)[0]
+
+    if prediction == 1:
+        st.error("High risk of heart Disease")
+    else:
+        st.success("low risk to heart Disease")
+
